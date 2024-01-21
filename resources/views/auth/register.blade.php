d57bc80f5de5eec25d752a9d30e4c882c00f61fd
Taikez <89579973+Taikez@users.noreply.github.com>
2024-01-21 11:49:53 +0700

    fix error message color for register & login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -37,7 +37,7 @@
                                     <label for="name" class="form-label">{{ __('Name') }}</label>
                                     <input id="name" class="form-control" type="text" name="name"
                                         value="{{ old('name') }}" required autofocus autocomplete="name" />
-                                    <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                                    <x-input-error :messages="$errors->get('name')" class="mt-2 text-danger" />
                                 </div>
 
                                 <!-- Email Address -->
@@ -45,7 +45,7 @@
                                     <label for="email" class="form-label">{{ __('Email') }}</label>
                                     <input id="email" class="form-control" type="email" name="email"
                                         value="{{ old('email') }}" required autocomplete="username" />
-                                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                    <x-input-error :messages="$errors->get('email')" class="mt-2 text-danger" />
                                 </div>
 
                                 <!-- Phone Number -->
@@ -53,7 +53,7 @@
                                     <label for="phoneNumber" class="form-label">{{ __('Phone Number') }}</label>
                                     <input id="phoneNumber" class="form-control" type="text" name="phoneNumber"
                                         value="{{ old('phoneNumber') }}" required autocomplete="username" />
-                                    <x-input-error :messages="$errors->get('phoneNumber')" class="mt-2" />
+                                    <x-input-error :messages="$errors->get('phoneNumber')" class="mt-2 text-danger" />
                                 </div>
 
                                 <!-- Password -->
@@ -61,7 +61,7 @@
                                     <label for="password" class="form-label">{{ __('Password') }}</label>
                                     <input id="password" class="form-control" type="password" name="password" required
                                         autocomplete="new-password" />
-                                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                                    <x-input-error :messages="$errors->get('password')" class="mt-2 text-danger" />
                                 </div>
 
                                 <!-- Confirm Password -->
@@ -70,7 +70,7 @@
                                         class="form-label">{{ __('Confirm Password') }}</label>
                                     <input id="password_confirmation" class="form-control" type="password"
                                         name="password_confirmation" required autocomplete="new-password" />
-                                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2 text-danger" />
                                 </div>
 
                                 <div class="text-center justify-end mb-3">
